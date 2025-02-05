bec50359e5d686489896eff7ea7c06458216af15
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 13:28:13 -0300

    OCP
    Adicionando exercicio de Open Closed Principle

---
+++ Exercicios/Exercicios/SOLID/OCP/Solucao/Solucao.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios.Exercicio_2.Solucao
+{
+  
+
+  
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override double Area {
+            get
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+       
+    }
+
+    public class Square: Shape
+    {
+        private int _height;
+        private int _width;
+
+        public int Height
+        {
+            get { return _height; }
+            set {
+                _height = value;
+                    _width = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set
+            {
+                _height = value;
+                _width = value;
+            }
+        }
+
+
+        public override double Area
+        {
+            get
+            {
+                return Width * Height;
+            }
+        }
+    }
+    public abstract class Shape
+    {
+        public virtual double Area { get; }
+    }
+
+    
+}
---
+++ Exercicios/Exercicios/SOLID/OCP/Violacao/OCP.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace Exercicios.Exercicio_2.Violacao
+{
+    public class AreaCalculator
+    {
+        public double CalculateArea(object shape)
+        {
+            if (shape is Rectangle rectangle)
+            {
+                return rectangle.Width * rectangle.Height;
+            }
+            else if (shape is Circle circle)
+            {
+                return Math.PI * Math.Pow(circle.Radius, 2);
+            }
+            
+            throw new ArgumentException("Invalid shape type");
+        }
+    }
+
+    public class Rectangle
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    public class Circle
+    {
+        public double Radius { get; set; }
+    }
+}
