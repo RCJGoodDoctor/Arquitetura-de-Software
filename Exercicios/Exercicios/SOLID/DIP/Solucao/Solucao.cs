85d87f5205ff83606585273e03501106f770bfe2
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 13:31:33 -0300

    DIP
    Adicionando Exercicio de Dependency Inversion Principle

---
+++ Exercicios/Exercicios/SOLID/DIP/Solucao/Solucao.cs
@@ -0,0 +1,43 @@
+﻿
+
+namespace Exercicios.Exercicio_5.Solucao
+{
+    public class BusinessLogic
+    {
+        private readonly ISqlDatabase _database;
+
+        public BusinessLogic(ISqlDatabase data)
+        {
+            _database = data;
+        }
+
+        public void AddData(string data)
+        {
+            _database.Save(data);
+        }
+    }
+
+    public class SQLServer : ISqlDatabase
+    {
+        public void Save(string data)
+        {
+            Console.WriteLine($"Saving '{data}' to SQLServer database");
+        }
+    }
+
+    public class Oracle : ISqlDatabase
+    {
+        public void Save(string data)
+        {
+            Console.WriteLine($"Saving '{data}' to SQLOracle database");
+        }
+
+    }
+
+    public interface ISqlDatabase
+    {
+        void Save(string data);
+    }
+
+
+}
---
+++ Exercicios/Exercicios/SOLID/DIP/Violacao/Violacao.cs
@@ -0,0 +1,33 @@
+﻿using Exercicios.Exercicio_5.Solucao;
+
+public class BusinessLogic
+{
+    private readonly SQLServer _database;
+
+    public BusinessLogic()
+    {
+        _database = new SQLServer();
+    }
+
+    public void AddData(string data)
+    {
+        _database.Save(data);
+    }
+}
+
+public class SQLServer 
+{
+    public void Save(string data)
+    {
+        Console.WriteLine($"Saving '{data}' to SQLServer database");
+    }
+}
+
+
+public class Oracle 
+{
+    public void Save(string data)
+    {
+        Console.WriteLine($"Saving '{data}' to SQLOracle database");
+    }
+}
