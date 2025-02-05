a0895ded88fdb888f50dc160e5fca575a97afa27
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 13:47:32 -0300

    adicionando console

---
+++ Exercicios/Console/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicios.Patterns.Abstract_Factory;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Escolha a o tipo de documento:");
+Console.WriteLine("------------------------");
+
+Console.WriteLine("1 - HTML");
+Console.WriteLine("2 - PDF");
+
+
+var opcao = Console.ReadKey();
+
+Console.WriteLine("");
+Console.WriteLine("------------------------");
+Console.WriteLine("");
+
+
+
+IDocumentFactory factory;
+switch (opcao.KeyChar)
+{
+    case '1':
+        factory = new HTMLFactory();
+        break;
+    case '2':
+        factory = new PDFFactory();
+        break;
+    default: throw new Exception("erro");
+
+}
+IHeader header = factory.CreateHeader();
+IRoster roster = factory.CreateRoster();
+IParagraph paragraph = factory.CreateParagraph();
+
+header.renderize();
+paragraph.renderize();
+roster.renderize();
+Console.ReadKey();
+Console.Clear();
