75f9ea3d557e7995327536c2b6eeb14b742e1460
Ricardo Junior <ricardo08jr@hotmail.com>
2025-02-05 13:29:28 -0300

    LSP
    Adicionando exercicio de Liskov Substitution Principle

---
+++ Exercicios/Exercicios/SOLID/LSP/Solucao/Solucao.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Exercicios.Exercicio_3.Solucao
+{
+    public abstract class Bird
+    {    
+        public virtual void Eat()
+        {
+            Console.WriteLine("Eating seeds");
+        }
+    }
+
+    public class FlyingBird : Bird {
+        public virtual void Fly()
+        {
+            Console.WriteLine("Flying high!");
+        }
+
+    }
+
+
+
+
+    public class Penguin : Bird
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Eating fish");
+        }
+    }
+}
---
+++ Exercicios/Exercicios/SOLID/LSP/Violacao/LSP.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Exercicios.Exercicio_3.Violacao
+{
+    public class Bird
+    {
+        public virtual void Fly()
+        {
+            Console.WriteLine("Flying high!");
+        }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("Eating seeds");
+        }
+    }
+
+    public class Penguin : Bird
+    {
+        public override void Fly()
+        {
+            throw new NotImplementedException("Penguins can't fly!");
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Eating fish");
+        }
+    }
+}
